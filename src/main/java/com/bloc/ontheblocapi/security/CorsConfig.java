3cd312b98a8f93a2774f7d01af8dfcce3bbdb7d9
Justin Sexton <justin.sexton@valic.com>
2019-04-14 10:26:15 -0500

    Hopefully fixed cors issue

---
+++ src/main/java/com/bloc/ontheblocapi/security/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.bloc.ontheblocapi.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedOrigins("localhost:3000")
+                .allowedMethods("POST", "DELETE", "GET")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
