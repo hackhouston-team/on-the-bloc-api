f065c7407040bbd9bbe4aa1f593b00d1fb0987ac
Justin Sexton <justin.sexton@valic.com>
2019-04-14 10:28:25 -0500

    Hopefully fixed cross origin issue

--- src/main/java/com/bloc/ontheblocapi/security/CorsConfig.java
+++ src/main/java/com/bloc/ontheblocapi/security/CorsConfig.java
@@ -13,6 +13,7 @@ public class CorsConfig extends WebMvcConfigurerAdapter {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedOrigins("localhost:3000")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("POST", "DELETE", "GET")
                 .allowCredentials(false).maxAge(3600);
     }
