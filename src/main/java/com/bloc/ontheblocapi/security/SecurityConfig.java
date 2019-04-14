56a9b4d5d1c181daf27c8fb60b26eeb0990067cf
Justin Sexton <justin.sexton@valic.com>
2019-04-14 10:00:54 -0500

    disabled cors all together

--- src/main/java/com/bloc/ontheblocapi/security/SecurityConfig.java
+++ src/main/java/com/bloc/ontheblocapi/security/SecurityConfig.java
@@ -10,6 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().disable().cors().disable();
     }
 }
